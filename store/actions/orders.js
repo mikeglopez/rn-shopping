9871006c6abcef0a7786b0884682297182ec93c4
Mike <mikeglopez.91@gmail.com>
2020-05-13 17:33:44 -0500

    Fetch stored orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,40 @@
+import Order from '../../models/order';
+
 export const ADD_ORDER = 'ADD_ORDER';
+export const SET_ORDERS = 'SET_ORDERS';
+
+export const fetchOrders = () => {
+  return async (dispatch) => {
+    // any async code
+    try {
+      const response = await fetch(
+        'https://rn-cg-shopping.firebaseio.com/orders/u1.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const resData = await response.json();
+      const loadedOrders = [];
+
+      for (const key in resData) {
+        loadedOrders.push(
+          new Order(
+            key,
+            resData[key].cartItems,
+            resData[key].totalAmount,
+            new Date(resData[key].date)
+          )
+        );
+      }
+      dispatch({ type: SET_ORDERS, orders: loadedOrders });
+    } catch (err) {
+      // could send to custom analytics server
+      throw err;
+    }
+  };
+};
 
 export const addOrder = (cartItems, totalAmount) => {
   return async (dispatch) => {
