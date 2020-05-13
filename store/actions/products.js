71e237aa745cb9310c97471a367e9a2bc559c2fd
Mike <mikeglopez.91@gmail.com>
2020-05-13 15:28:25 -0500

    Add loading spinner and error handling

--- store/actions/products.js
+++ store/actions/products.js
@@ -8,27 +8,36 @@ export const SET_PRODUCTS = 'SET_PRODUCTS';
 export const fetchProducts = () => {
   return async (dispatch) => {
     // any async code
-    const response = await fetch(
-      'https://rn-cg-shopping.firebaseio.com/products.json'
-    );
-
-    const resData = await response.json();
-    const loadedProducts = [];
-
-    for (const key in resData) {
-      loadedProducts.push(
-        new Product(
-          key,
-          'u1',
-          resData[key].title,
-          resData[key].imageUrl,
-          resData[key].description,
-          resData[key].price
-        )
+    try {
+      const response = await fetch(
+        'https://rn-cg-shopping.firebaseio.com/products.json'
       );
-    }
 
-    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const resData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            'u1',
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      // could send to custom analytics server
+      throw err;
+    }
   };
 };
 
