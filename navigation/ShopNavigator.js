188a2ca30970b9d60c8df02a2df1a1ec2f60e114
Mike <mikeglopez.91@gmail.com>
2020-05-16 19:52:21 -0500

    Implement basic login screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
-import { createAppContainer } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -11,6 +11,7 @@ import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
 import UserProductsScreen from '../screens/user/UserProductsScreen';
 import EditProductScreen from '../screens/user/EditProductScreen';
+import AuthScreen from '../screens/user/AuthScreen';
 import Colors from '../constants/Colors';
 
 const defaultNavOptions = {
@@ -34,7 +35,7 @@ const ProductsNavigator = createStackNavigator(
   },
   {
     navigationOptions: {
-      drawerIcon: (drawerConfig) => (
+      drawerIcon: drawerConfig => (
         <Ionicons
           name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
           size={23}
@@ -52,7 +53,7 @@ const OrdersNavigator = createStackNavigator(
   },
   {
     navigationOptions: {
-      drawerIcon: (drawerConfig) => (
+      drawerIcon: drawerConfig => (
         <Ionicons
           name={Platform.OS === 'android' ? 'md-list' : 'ios-list'}
           size={23}
@@ -71,7 +72,7 @@ const AdminNavigator = createStackNavigator(
   },
   {
     navigationOptions: {
-      drawerIcon: (drawerConfig) => (
+      drawerIcon: drawerConfig => (
         <Ionicons
           name={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
           size={23}
@@ -96,4 +97,18 @@ const ShopNavigator = createDrawerNavigator(
   }
 );
 
-export default createAppContainer(ShopNavigator);
+const AuthNavigator = createStackNavigator(
+  {
+    Auth: AuthScreen
+  },
+  {
+    defaultNavigationOptions: defaultNavOptions
+  }
+);
+
+const MainNavigator = createSwitchNavigator({
+  Auth: AuthNavigator,
+  Shop: ShopNavigator
+});
+
+export default createAppContainer(MainNavigator);
